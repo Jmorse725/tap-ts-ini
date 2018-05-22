8868f28c818cc59494e5f4b3c524fb622fbc0070
Jonathan <jonathan@sbcotton.com>
2018-05-22 09:37:07 -0500

    First commit

---
+++ src/parse-ini.ts
@@ -0,0 +1,26 @@
+/** Parse MIME files using [Nodemailer.Mailparser](https://nodemailer.com/extras/mailparser/)
+ *
+ */ /** hack for https://github.com/TypeStrong/typedoc/issues/603 */
+//var test = {a:'', b:''};
+/* 
+Updated mailparser to 2.2.0 to avoid malicious getcookies module; see https://blog.npmjs.org/post/173526807575/reported-malicious-module-getcookies
+Consider mailparse (https://github.com/javascriptlove/mailparse) for the future, since mailparser will no longer be maintained. Mailparse does not yet have any TypeScript types available.
+*/
+import * as anyJson from 'any-json'
+//var mp = mailparser.MailParser; // low-level parser
+// higher-level parser (easier to use, not as efficient)
+
+import * as tapTypes from './tap-types'
+import { stringify } from 'querystring'
+import { AnyLengthString } from 'aws-sdk/clients/comprehend'
+
+/** Convert the Mime message into json */
+export function parseItem(test: Buffer) {
+  return anyJson.decode(test.toString('utf8'), 'ini').then(function(iniObj) {
+    let rec = new tapTypes.streamRecord()
+    rec.stream = 'ini'
+    rec.time_extracted = new Date()
+    rec.record = iniObj
+    return rec
+  })
+}
--- src/tap-ts-starter.ts
+++ src/tap-ts-starter.ts
@@ -2,28 +2,34 @@
 // import "core-js/fn/array.find"
 // ...
 export default class DummyClass {}
+/** hack for https://github.com/TypeStrong/typedoc/issues/603 */
 
-console.log(
-  'working!'
-) /** hack for https://github.com/TypeStrong/typedoc/issues/603 */
+const anyjson = require('any-json')
+// const eep = anyjson.decode('feild, name\ndummy,peter', 'csv')
+// eep.then(function(jsonobj){
+//   console.log(jsonobj)
+// })
 
+console.log('working!')
+//console.log(eep)
 /**
  * This module is the entry point for local execution as a Singer tap (see the [spec](https://github.com/singer-io/getting-started/blob/master/SPEC.md))
  */
 import * as configLoader from './tap-load-config'
-import * as parseMime from './parse-mime'
+import * as parseIni from './parse-ini'
 import * as scanDir from './scan-dir'
 
 /** random note */
 
 /** random note 2
-  * 
-  */
+ *
+ */
 configLoader
   .loadConfig()
   .then(function(configObjs) {
     // run scanDir using parseMime as the parser for each item
-    return scanDir.scanDir(configObjs, parseMime.parseItem)
+
+    return scanDir.scanDir(configObjs, parseIni.parseItem)
   })
   .catch(function(error) {
     // Handle errors
